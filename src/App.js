da0fcaac013542331e88b2636fbeb89531c12a9c
Alex Koffi <kouadiojeanalexkoffi@gmail.com>
2025-03-29 09:19:36 +0000

    feat: debut du checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import {Card} from 'react-bootstrap/Card';
+import Name from './Name';
+import Price from './Price';
+import Description from './Description';
+import Image from './Image';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const prénom = "Alex"; // Remplace par ton prénom ou laisse vide pour tester
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Image />
+          <Card.Title><Name /></Card.Title>
+          <Card.Subtitle className="mb-2 text-muted"><Price /></Card.Subtitle>
+          <Card.Text><Description /></Card.Text>
+        </Card.Body>
+      </Card>
+      {prénom ? (
+        <p>Bonjour, {prénom}!</p>
+      ) : (
+        <p>Bonjour !</p>
+      )}
+      {prénom && <img src="https://images.anandtech.com/doci/10194/X-T30_DSF4322.jpg" alt="Avatar" />}
     </div>
   );
 }
---
+++ src/product.js
@@ -0,0 +1,10 @@
+// src/product.js
+const product = {
+    name: "Produit fantastique",
+    price: "29.99€",
+    description: "Ce produit va changer votre vie !",
+    image: "https://images.anandtech.com/doci/10194/X-T30_DSF4322.jpg" // Remplace par l'URL de l'image de ton choix
+  };
+  
+  export default product;
+  
